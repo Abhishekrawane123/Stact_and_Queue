b9eb26a1340a19f2590aa197466f57b1e823cd63
pknowledge <abhishekrawane1195@gmail.com>
2021-11-19 22:21:44 +0530

    Added : Pushed the data

---
+++ src/com/bridgelabz/Stack/StackMain.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.Stack;
+
+public class StackMain {
+	
+	public static void main(String[] args) {
+		StackPractice practice = new StackPractice(5);
+		practice.push(10);
+		practice.push(20);
+		practice.push(90);
+		practice.push(10);
+		practice.push(20);
+		practice.push(90);
+		//practice.toPrint();
+	}
+
+}
